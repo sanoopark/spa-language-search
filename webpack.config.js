e922b8c73f30e0dbfe9049a841c807482dce9fca
sanoopark <sanoopark@gmail.com>
2022-04-16 04:13:26 +0900

    Build: Webpack, Babel 설정

---
+++ webpack.config.js
@@ -0,0 +1,78 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+const isDevMode = process.env.NODE_ENV.includes('dev');
+
+const plugins = [
+  new Dotenv({
+    path: '.env',
+  }),
+  new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, 'index.html'),
+  }),
+  new CleanWebpackPlugin(),
+];
+
+if (!isDevMode) {
+  plugins.push(new MiniCssExtractPlugin());
+}
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: path.resolve(__dirname, 'src/index.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/i,
+        exclude: /node_modules/,
+        use: ['ts-loader'],
+      },
+      {
+        test: /\.m?jsx?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              '@babel/transform-runtime',
+              '@babel/plugin-proposal-class-properties',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(sc|sa|c)ss$/i,
+        exclude: /node_modules/,
+        use: [
+          isDevMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins,
+  optimization: {
+    minimize: isDevMode ? false : true,
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  devtool: 'source-map',
+  devServer: {
+    historyApiFallback: true,
+    hot: true,
+  },
+};
